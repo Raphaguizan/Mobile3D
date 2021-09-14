6ae11bae7dbe41fcd0fc95530df1fe9e44930a74
DESKTOP-Q313TGL\rapha <raphaelguizan@gmail.com>
2021-09-14 18:29:20 -0300

    feat: Enemies and end Game

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,13 +8,42 @@ public class PlayerController : MonoBehaviour
     public Transform target;
     public float followTargetSpeed = 1f;
 
-    private float _posX;
+    [Header("Compare Tags")]
+    public string EnemyTag = "Enemy";
 
-    
+    private float _posX;
+    private bool _canRun;
+
+    private void Start()
+    {
+        StartGame();
+    }
+
     void Update()
     {
+        if (!_canRun) return;
+
         transform.Translate(Vector3.forward * Time.deltaTime * fwdSpeed);
         _posX = Mathf.Lerp(transform.position.x, target.position.x, followTargetSpeed * Time.deltaTime);
         transform.position = new Vector3(_posX, transform.position.y, transform.position.z);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag(EnemyTag))
+        {
+            EndGame();
+        }
+    }
+
+
+    private void StartGame()
+    {
+        _canRun = true;
+    }
+
+    private void EndGame()
+    {
+        _canRun = false;
     }
 }
